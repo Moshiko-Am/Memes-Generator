5065ddb7698a104a7d4a14e82e7e699b2d9fcfd1
Moshiko Amsalem <moshiko14691@gmail.com>
2021-06-10 15:33:12 +0300

    start of responsive

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,71 +2,174 @@
 var gCanvas;
 var gCtx;
 var gCurrImgIdx;
-var gCurrTxt = '';
-var gLineWidth = 225;
-var gLineHeight = 56;
+var gLineHeight;
+var gLineWidth;
+var gCurrFont = 'Impact';
 
 function init() {
   gCanvas = document.querySelector('.my-canvas');
   gCtx = gCanvas.getContext('2d');
-  renderGallery();
+  resizeCanvas();
+  renderGallery(gImgs);
 }
 
-function drawImg(idx) {
-  //   gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
-  gCurrImgIdx = idx;
-  //   var elGallery = document.querySelector('.gallery-container');
-  //   elGallery.hidden = true;
-  var img = new Image();
-  img.src = gImgs[gCurrImgIdx].url;
-  gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
-}
-
-function drawTxt(txt) {
-  gSelectedLine = 0;
-  gMeme.lines.forEach(() => {
-    gCtx.lineWidth = 2;
-    gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = gMeme.lines[gSelectedLine].color;
-    gCtx.font = gMeme.lines[gSelectedLine].size + 'px Impact';
-    gCtx.textAlign = gMeme.lines[gSelectedLine].align;
-    gCtx.fillText(txt, gLineWidth, gLineHeight);
-    gCtx.strokeText(txt, gLineWidth, gLineHeight);
-  });
-  if (gMeme.gSelectedLine < gMeme.lines.length - 1) {
-    gMeme.gSelectedLine++;
-  }
-}
-
-function onSetNewInput() {
-  var elText = document.querySelector('input[name=meme-text]').value;
-  //   drawImg(gCurrImgIdx);
-  setInput(elText);
-  drawTxt(elText);
-}
-
-function renderGallery() {
-  var strHTMLs = gImgs.map((img, idx) => {
-    return `<img src="${img.url}" data-idx=${idx} style="width: 150px;" onclick="drawImg(${idx})" class="gallery-img"/>`;
+function renderGallery(images) {
+  var strHTMLs = images.map((img, idx) => {
+    return `<img src="${img.url}" data-idx=${idx} style="width: 150px;" onclick="displayImage(${idx})" class="gallery-img"/>`;
   });
   var elGallery = document.querySelector('.gallery-container');
   elGallery.innerHTML = strHTMLs.join('');
 }
 
+function displayImage(idx) {
+  gCurrImgIdx = idx;
+  var elSearch = document.querySelector('.gallery-filter');
+  var elGallery = document.querySelector('.gallery-container');
+  var elMain = document.querySelector('.main-content');
+  elMain.classList.toggle('hide');
+  elSearch.classList.toggle('hide');
+  elGallery.classList.toggle('hide');
+  resizeCanvas();
+  drawImg(gCurrImgIdx);
+}
+
+function drawImg(idx) {
+  gCurrImgIdx = idx;
+  var img = new Image();
+  img.src = gImgs[gCurrImgIdx].url;
+  gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+}
+
+function drawTxt() {
+  var txt = gMeme.lines[gMeme.selectedLineIdx].txt;
+  gCtx.lineWidth = 2;
+  gCtx.strokeStyle = 'black';
+  gCtx.fillStyle = gMeme.lines[gMeme.selectedLineIdx].color;
+  gCtx.font =
+    gMeme.lines[gMeme.selectedLineIdx].size +
+    'px ' +
+    gMeme.lines[gMeme.selectedLineIdx].font;
+  gCtx.textAlign = gMeme.lines[gMeme.selectedLineIdx].align;
+  gCtx.fillText(
+    txt,
+    gMeme.lines[gMeme.selectedLineIdx].lineWidth,
+    gMeme.lines[gMeme.selectedLineIdx].lineHeight
+  );
+  gCtx.strokeText(
+    txt,
+    gMeme.lines[gMeme.selectedLineIdx].lineWidth,
+    gMeme.lines[gMeme.selectedLineIdx].lineHeight
+  );
+}
+
+function onSetNewInput() {
+  var elText = document.querySelector('input[name=meme-text]');
+  //   drawImg(gCurrImgIdx);
+  setInput(elText.value);
+  drawTxt();
+  elText.value = '';
+}
+
 function onTextSize(diff) {
   drawImg(gCurrImgIdx);
   setTextSize(diff);
-  drawTxt(gMeme.lines[gSelectedLine].txt);
+  renderLines();
+}
+
+function renderLines() {
+  gMeme.selectedLineIdx = 0;
+  gMeme.lines.forEach(() => {
+    drawTxt(gMeme.lines[gMeme.selectedLineIdx].txt);
+    if (gMeme.selectedLineIdx < gMeme.lines.length - 1) {
+      gMeme.selectedLineIdx++;
+    }
+  });
+  drawRect();
 }
 
 function onTextMove(diff) {
   drawImg(gCurrImgIdx);
-  gLineHeight += diff;
-  drawTxt(gMeme.lines[gSelectedLine].txt);
+  moveText(diff);
+  renderLines();
 }
 
-function onAddNewLine() {
-  createNewLine();
+function onTextSide(diff) {
+  textSide(diff);
+  drawImg(gCurrImgIdx);
+  renderLines();
+}
+
+function onSwitchLine() {
   gSelectedLine++;
-  drawTxt(gMeme.lines[gSelectedLine].txt);
+  gMeme.selectedLineIdx = gSelectedLine;
+
+  if (gMeme.selectedLineIdx > gMeme.lines.length - 1) {
+    gMeme.selectedLineIdx = 0;
+    gSelectedLine = 0;
+  }
+  var elText = document.querySelector('input[name=meme-text]');
+  elText.value = gMeme.lines[gSelectedLine].txt;
+  drawImg(gCurrImgIdx);
+  renderLines();
+}
+
+function onRemoveTxt() {
+  removeTxt();
+  gLineHeight = 50;
+  drawImg(gCurrImgIdx);
+  renderLines();
+}
+
+function resizeCanvas() {
+  const elContainer = document.querySelector('.canvas-container');
+  gCanvas.width = elContainer.offsetWidth;
+  gCanvas.height = elContainer.offsetHeight;
+  gLineHeight = gCanvas.height - (gCanvas.height - 50);
+  gLineWidth = gCanvas.width / 2;
+}
+
+function drawRect() {
+  gCtx.beginPath();
+  gCtx.rect(
+    0,
+    gMeme.lines[gSelectedLine].lineHeight - gMeme.lines[gSelectedLine].size,
+    gCanvas.width,
+    gMeme.lines[gSelectedLine].size + 10
+  );
+  gCtx.strokeStyle = 'white';
+  gCtx.stroke();
+}
+
+function onTextAlign(val) {
+  textAlign(val);
+  drawImg(gCurrImgIdx);
+  renderLines();
+}
+
+function onFontChange(val) {
+  gCurrFont = val;
+  fontChange(gCurrFont);
+  drawImg(gCurrImgIdx);
+  renderLines();
+}
+
+function onSetFilter(txt) {
+  setFilter(txt);
+  var images = getImagesForDisplay();
+  renderGallery(images);
+}
+
+function biggerSize(elBtn) {
+  var font = getComputedStyle(elBtn, null).fontSize;
+  font = parseInt(font);
+  elBtn.style.fontSize = font + 2 + 'px';
+}
+
+function downloadImg(elLink) {
+  var imgContent = gCanvas.toDataURL('image/jpeg');
+  elLink.href = imgContent;
+}
+
+function showHome() {
+  location.reload();
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,60 +1,110 @@
 'use strict';
 var gId = 0;
-var gKeywords;
 var gSelectedLine = 0;
+var gFilterBy = '';
+
+var gCurrTxt = '';
 
 var gImgs = [
-  { id: gId++, url: '../img/1.jpg' },
-  { id: gId++, url: '../img/2.jpg' },
-  { id: gId++, url: '../img/3.jpg' },
-  { id: gId++, url: '../img/4.jpg' },
-  { id: gId++, url: '../img/5.jpg' },
-  { id: gId++, url: '../img/6.jpg' },
-  { id: gId++, url: '../img/7.jpg' },
-  { id: gId++, url: '../img/8.jpg' },
-  { id: gId++, url: '../img/9.jpg' },
-  { id: gId++, url: '../img/10.jpg' },
-  { id: gId++, url: '../img/11.jpg' },
-  { id: gId++, url: '../img/12.jpg' },
-  { id: gId++, url: '../img/13.jpg' },
-  { id: gId++, url: '../img/14.jpg' },
-  { id: gId++, url: '../img/15.jpg' },
-  { id: gId++, url: '../img/16.jpg' },
-  { id: gId++, url: '../img/17.jpg' },
-  { id: gId++, url: '../img/18.jpg' },
+  { id: gId++, url: 'img/1.jpg', keyword: ['trump', 'president'] },
+  { id: gId++, url: 'img/2.jpg', keyword: ['dogs', 'puppy', 'siblings'] },
+  { id: gId++, url: 'img/3.jpg', keyword: ['baby', 'puppy', 'siblings'] },
+  {
+    id: gId++,
+    url: 'img/4.jpg',
+    keyword: ['cat', 'computer', 'keyboard', 'tired'],
+  },
+  { id: gId++, url: 'img/5.jpg', keyword: ['baby', 'success'] },
+  { id: gId++, url: 'img/6.jpg', keyword: ['history', 'crazy', 'hair'] },
+  { id: gId++, url: 'img/7.jpg', keyword: ['baby', 'surprise'] },
+  { id: gId++, url: 'img/8.jpg', keyword: ['lister', 'happy'] },
+  {
+    id: gId++,
+    url: 'img/9.jpg',
+    keyword: ['baby', 'chinese', 'funny', 'laughing'],
+  },
+  {
+    id: gId++,
+    url: 'img/10.jpg',
+    keyword: ['obama', 'president', 'laughing', 'funny'],
+  },
+  {
+    id: gId++,
+    url: 'img/11.jpg',
+    keyword: ['wrestling', 'siblings', 'brother'],
+  },
+  { id: gId++, url: 'img/12.jpg', keyword: ['got you', 'glasses', 'tv'] },
+  { id: gId++, url: 'img/13.jpg', keyword: ['cheers', 'got you'] },
+  { id: gId++, url: 'img/14.jpg', keyword: ['matrix', 'teach'] },
+  { id: gId++, url: 'img/15.jpg', keyword: ['lord', 'rings', 'teach'] },
+  { id: gId++, url: 'img/16.jpg', keyword: ['omg', 'surprise'] },
+  { id: gId++, url: 'img/17.jpg', keyword: ['putin', 'president'] },
+  { id: gId++, url: 'img/18.jpg', keyword: ['cartoon'] },
 ];
 
 var gMeme = {
   selectedImgId: 0,
-  selectedLineIdx: gSelectedLine,
-  lines: [
-    {
-      txt: [],
-      size: 40,
-      align: 'center',
-      color: 'white',
-    },
-  ],
+  selectedLineIdx: 0,
+  lines: [],
 };
-
-function setInput(txt) {
-  if (!gMeme.lines[gSelectedLine].txt) {
-    gMeme.lines[gSelectedLine].txt = txt;
-  } else {
-    gMeme.lines[gSelectedLine].txt.push(txt);
-  }
-}
 
 function setTextSize(diff) {
   gMeme.lines[gSelectedLine].size += diff;
 }
 
-function createNewLine() {
+function moveText(diff) {
+  gMeme.lines[gSelectedLine].lineHeight += diff;
+}
+
+function textSide(diff) {
+  gMeme.lines[gSelectedLine].lineWidth += diff;
+}
+
+function setInput(txt) {
   var newLine = {
-    txt: [],
+    txt,
     size: 40,
+    font: 'Impact',
     align: 'center',
     color: 'white',
+    lineHeight: gLineHeight,
+    lineWidth: gLineWidth,
   };
   gMeme.lines.push(newLine);
+  gMeme.selectedLineIdx = gMeme.lines.length - 1;
+  gSelectedLine = gMeme.selectedLineIdx;
+  switch (gLineHeight) {
+    case 50:
+      gLineHeight = gCanvas.height - 20;
+      break;
+    case gCanvas.height - 20:
+      gLineHeight = gCanvas.height / 2;
+      break;
+  }
+}
+
+function removeTxt() {
+  gMeme.lines.splice(gSelectedLine, 1);
+}
+
+function textAlign(val) {
+  gMeme.lines[gSelectedLine].align = val;
+}
+
+function fontChange(val) {
+  gCurrFont = val;
+  gMeme.lines[gSelectedLine].font = gCurrFont;
+}
+
+function setFilter(filterBy) {
+  gFilterBy = filterBy;
+}
+
+function getImagesForDisplay() {
+  var images = gImgs.filter((image) => {
+    return image.keyword.some((word) => {
+      return word.includes(gFilterBy);
+    });
+  });
+  return images;
 }
